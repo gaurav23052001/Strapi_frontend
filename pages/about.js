4221e7ab7e05d9923875ef0094aa7e5c2c169fed
gaurav23052001 <mishragaurav8446@gmailcom>
2023-07-04 11:24:58 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      bhut mast
+    </div>
+  )
+}
+
+export default About
---
+++ pages/products.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>
+      appple
+    </div>
+  )
+}
+
+export default Products
