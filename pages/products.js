89c41fdcddc63ad4bddc3be6ba673ce68f7be6d5
Simulanis-Dev-Gaurav <gauravmishra@simulanis.com>
2023-07-06 19:31:53 +0530

    slug error

--- pages/products.js
+++ pages/products.js
@@ -1,80 +1,57 @@
-import { headers } from 'next/dist/client/components/headers';
-import React from 'react'
+import React from 'react';
+import axios from "axios";
+import Link from 'next/link';
+import Navbar from '../components/navbar'
 
 const products = (props) => {
   return (
     <div>
-      
       <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap w-full mb-20">
       <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">My shop </h1>
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
         <div className="h-1 w-20 bg-indigo-500 rounded"></div>
       </div>
       <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
     </div>
     <div className="flex flex-wrap -m-4">
-      {props.products.data.map((item)=>{
-        return(
-      <div className="xl:w-1/4 md:w-1/2 p-4">
+      {props.Products.map((item)=>{
+        return (
+      <div key={item.attributes.slug} className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
-        <img
-  className="h-40 rounded w-full object-cover object-center mb-6"
-  src={item.attributes.Image.data && item.attributes.Image.data.attributes && item.attributes.Image.data.attributes.name} alt="content"/>
-
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <img className="h-96 rounded m-auto mb-8" src={item.attributes.Image && item.attributes.Image.data && item.attributes.Image.data[0].attributes.url} alt="content"/>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{item.attributes.catagory}</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">{item.attributes.Tittle}</h2>
+          <div className=" bg-red-800 bg-purple-800 bg-green-800"></div>
+          <button className={"border-2 border-gray-300 ml-1 rounded-full w-6 h-6 focus:outline-none " + `bg-${item.attributes.color}-800`}></button>
+          <p className="leading-relaxed text-base"> {item.attributes.Discripation}</p>
+          <Link href={`/product/${item.attributes.slug}`}><button className="my-2 text-white bg-indigo-500 border-0 py-1 md:py-2 px-2 md:px-4 focus:outline-none hover:bg-indigo-600 rounded text-sm">Buy Now</button></Link>
         </div>
       </div>
       )
     })}
-      <div className="xl:w-1/4 md:w-1/2 p-4">
-        <div className="bg-gray-100 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content"/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-        </div>
-      </div>
-      <div className="xl:w-1/4 md:w-1/2 p-4">
-        <div className="bg-gray-100 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content"/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-        </div>
-      </div>
-      <div className="xl:w-1/4 md:w-1/2 p-4">
-        <div className="bg-gray-100 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content"/>
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-        </div>
-      </div>
     </div>
   </div>
 </section>
-
     </div>
   )
 }
 
-export async function getServerSideProps(context) {
-  let headers = { Authorization: "Bearer 399124295508cf8ecfa448e28dd3a9f115e01a0e7034ea260134bf3cffe965711e6a6cbb891723b6ba15134136566bc2ac1f468e18230d39eb11da38b07e5fbffbbb76d8882c5fc9a1a2958aeeb1dc8af3d4bcfac37d1a24377bba680772bbb24c92db13719bbde8e70e6a190e7629913a6e3c090035e649b760c2ebb877fd80" };
+export async function getServerSideProps() {
+  let headers = {"Authorization": "bearer 399124295508cf8ecfa448e28dd3a9f115e01a0e7034ea260134bf3cffe965711e6a6cbb891723b6ba15134136566bc2ac1f468e18230d39eb11da38b07e5fbffbbb76d8882c5fc9a1a2958aeeb1dc8af3d4bcfac37d1a24377bba680772bbb24c92db13719bbde8e70e6a190e7629913a6e3c090035e649b760c2ebb877fd80" };
 
-  const res = await fetch('http://localhost:1337/api/products?populate=*', { headers: headers });
-  const data = await res.json();
+const res = await axios.get('http://127.0.0.1:1337/api/products',{ headers });
 
-  // Return the data as props
+  const Products =res.data.data;
+
   return {
-    props: { products: data },
+    props: {
+      Products,
+    },
   };
 }
 
 
+
 export default products
-
-
